232cc86e1bfcf5353114d916a9a54cc15ba3cead
Helton Bustos <heltonsmith@hotmail.com>
2022-05-20 14:24:07 -0400

    commit inicial

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'nuevo/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "nuevo#index"
+end
